04f0ada87bf8a68cbe22052d56b0bbf633c61f0b
dakshayh <akshayh380@gmail.com>
2018-03-07 23:52:11 +0530

    Create output_list.py

---
+++ output_list.py
@@ -0,0 +1,30 @@
+def show_output(output_list):
+    output_list_len = len(output_list)
+    figure = plt.figure()
+    for i in range(output_list_len):
+        current_dict = output_list[i]
+        current_img = current_dict["img"]
+        current_xlabel = current_dict["xlabel"]
+        current_ylabel = current_dict["ylabel"]
+        current_xtick = current_dict["xtick"]
+        current_ytick = current_dict["ytick"]
+        current_title = current_dict["title"]
+        current_type = current_dict["type"]
+        current_plot = figure.add_subplot(1, output_list_len, i+1)
+        if current_type == "gray":
+            current_plot.imshow(current_img, cmap = plt.get_cmap('gray'))
+            current_plot.set_title(current_title)
+            current_plot.set_xticks(current_xtick)
+            current_plot.set_yticks(current_ytick)
+            current_plot.set_xlabel(current_xlabel)
+            current_plot.set_ylabel(current_ylabel)
+        elif current_type == "histogram":
+            current_plot.plot(current_img, color = "black")
+            current_plot.set_xlim([0,260])
+            current_plot.set_title(current_title)
+            current_plot.set_xlabel(current_xlabel)
+            current_plot.set_ylabel(current_ylabel)            
+            ytick_list = [int(i) for i in current_plot.get_yticks()]
+            current_plot.set_yticklabels(ytick_list,rotation = 90)
+    
+    plt.show()
