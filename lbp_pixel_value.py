4d23709a7732d4d2b0ed5c120a2a4d16f63958cf
dakshayh <akshayh380@gmail.com>
2018-03-07 23:51:02 +0530

    Create lbp_pixel_value.py

---
+++ lbp_pixel_value.py
@@ -0,0 +1,27 @@
+import os
+import get_pixel
+
+def lbp_calculate(img, x, y):
+    '''
+     64 | 128 |   1
+    ----------------
+     32 |   0 |   2
+    ----------------
+     16 |   8 |   4    
+    '''    
+    center = img[x][y]
+    val_ar = []
+    val_ar.append(get_pixel(img, center, x-1, y+1))     # top_right
+    val_ar.append(get_pixel(img, center, x, y+1))       # right
+    val_ar.append(get_pixel(img, center, x+1, y+1))     # bottom_right
+    val_ar.append(get_pixel(img, center, x+1, y))       # bottom
+    val_ar.append(get_pixel(img, center, x+1, y-1))     # bottom_left
+    val_ar.append(get_pixel(img, center, x, y-1))       # left
+    val_ar.append(get_pixel(img, center, x-1, y-1))     # top_left
+    val_ar.append(get_pixel(img, center, x-1, y))       # top
+    
+    power_val = [1, 2, 4, 8, 16, 32, 64, 128]
+    val = 0
+    for i in range(len(val_ar)):
+        val += val_ar[i] * power_val[i]
+    return val    
