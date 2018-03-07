8c82af39879bff4b10f5fc9713d6b30a2fd4cedd
dakshayh <akshayh380@gmail.com>
2018-03-07 23:54:44 +0530

    Update LBP.py

--- LBP.py
+++ LBP.py
@@ -2,73 +2,8 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import os
-
-def get_pixel(img, center, x, y):
-    new_value = 0
-    try:
-        if img[x][y] >= center:
-            new_value = 1
-    except:
-        pass
-    return new_value
-
-
-def lbp_calculated_pixel(img, x, y):
-    '''
-     64 | 128 |   1
-    ----------------
-     32 |   0 |   2
-    ----------------
-     16 |   8 |   4    
-    '''    
-    center = img[x][y]
-    val_ar = []
-    val_ar.append(get_pixel(img, center, x-1, y+1))     # top_right
-    val_ar.append(get_pixel(img, center, x, y+1))       # right
-    val_ar.append(get_pixel(img, center, x+1, y+1))     # bottom_right
-    val_ar.append(get_pixel(img, center, x+1, y))       # bottom
-    val_ar.append(get_pixel(img, center, x+1, y-1))     # bottom_left
-    val_ar.append(get_pixel(img, center, x, y-1))       # left
-    val_ar.append(get_pixel(img, center, x-1, y-1))     # top_left
-    val_ar.append(get_pixel(img, center, x-1, y))       # top
-    
-    power_val = [1, 2, 4, 8, 16, 32, 64, 128]
-    val = 0
-    for i in range(len(val_ar)):
-        val += val_ar[i] * power_val[i]
-    return val    
-
-
-def show_output(output_list):
-    output_list_len = len(output_list)
-    figure = plt.figure()
-    for i in range(output_list_len):
-        current_dict = output_list[i]
-        current_img = current_dict["img"]
-        current_xlabel = current_dict["xlabel"]
-        current_ylabel = current_dict["ylabel"]
-        current_xtick = current_dict["xtick"]
-        current_ytick = current_dict["ytick"]
-        current_title = current_dict["title"]
-        current_type = current_dict["type"]
-        current_plot = figure.add_subplot(1, output_list_len, i+1)
-        if current_type == "gray":
-            current_plot.imshow(current_img, cmap = plt.get_cmap('gray'))
-            current_plot.set_title(current_title)
-            current_plot.set_xticks(current_xtick)
-            current_plot.set_yticks(current_ytick)
-            current_plot.set_xlabel(current_xlabel)
-            current_plot.set_ylabel(current_ylabel)
-        elif current_type == "histogram":
-            current_plot.plot(current_img, color = "black")
-            current_plot.set_xlim([0,260])
-            current_plot.set_title(current_title)
-            current_plot.set_xlabel(current_xlabel)
-            current_plot.set_ylabel(current_ylabel)            
-            ytick_list = [int(i) for i in current_plot.get_yticks()]
-            current_plot.set_yticklabels(ytick_list,rotation = 90)
-    
-    plt.show()
+import get_pixel
+import lbp_calculate
 
 
 def main():
