68e1fd2876f56c6ba6d37b8a5a4433790ace320a
dakshayh <akshayh380@gmail.com>
2018-03-09 15:06:52 +0530

    Create CNN_initialize.py

---
+++ CNN_initialize.py
@@ -0,0 +1,22 @@
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+
+classifier = Sequential()
+
+classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
+
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
+
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+classifier.add(Flatten())
+
+classifier.add(Dense(output_dim = 128, activation = 'relu'))
+
+classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
