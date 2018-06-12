1bdb237e36d8d1bf6285f44fda6ff9da0e946b3e
dakshayh <akshayh380@gmail.com>
2018-06-12 10:51:26 +0530

    Delete caffe_example.py

--- caffe_example.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
-import caffe as cf
-
-cf.set_mode_cpu()
-#loading the network
-net=cf.Net('conv.prototxt',cf.TEST)
-print (net.blobs['conv'].data.shape)
